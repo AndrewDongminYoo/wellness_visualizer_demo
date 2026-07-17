c74f127dea747ea18ba76be86f8a752a18259e19
Dongmin, Yu <ydm2790@gmail.com>
2026-07-17 09:48:15 +0900

    chore: remove unused app and localization files

--- lib/main_production.dart
+++
@@ -1,6 +0,0 @@
-import 'package:wellness_visualizer_demo/app/app.dart';
-import 'package:wellness_visualizer_demo/bootstrap.dart';
-
-Future<void> main() async {
-  await bootstrap(() => const App());
-}
